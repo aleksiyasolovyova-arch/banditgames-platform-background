60fbdf84a19bf66317530832a4973aabc7fba7c2
Radu-Matei Enciu <radu-matei.enciu@student.kdg.be>
2025-11-24 16:28:00 +0100

    Checked player BC

--- src/main/java/team11/platform_backend/player/domain/player/UnlockedAchievement.java
+++ src/main/java/team11/platform_backend/player/domain/player/UnlockedAchievement.java
@@ -1,7 +1,16 @@
 package team11.platform_backend.player.domain.player;
 import java.time.LocalDateTime;
 
+
 public record UnlockedAchievement(
         AchievementId achievementId,
         LocalDateTime unlockedAt
-) {}
+) {
+    public UnlockedAchievement {
+        if (achievementId == null)
+            throw new IllegalArgumentException("achievementId cannot be null");
+        if (unlockedAt == null)
+            throw new IllegalArgumentException("unlockedAt cannot be null");
+    }
+}
+
