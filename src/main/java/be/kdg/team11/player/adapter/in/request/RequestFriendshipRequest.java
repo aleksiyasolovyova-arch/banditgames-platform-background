633d2b742e4619c6c4fdba1e0b51884fcfa8c657
Radu-Matei Enciu <radu-matei.enciu@student.kdg.be>
2025-12-23 23:23:36 +0100

    commented out

--- src/main/java/be/kdg/team11/player/adapter/in/request/RequestFriendshipRequest.java
+++ src/main/java/be/kdg/team11/player/adapter/in/request/RequestFriendshipRequest.java
@@ -1,8 +1,9 @@
 package be.kdg.team11.player.adapter.in.request;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record RequestFriendshipRequest(
-        @NotNull(message = "Recipient ID cannot be null")
+        @NotEmpty(message = "Recipient username cannot be empty")
         String recipientUsername
 ) {}
--- src/main/java/be/kdg/team11/readmodel/service/game/GameModelMapper.java
+++ src/main/java/be/kdg/team11/readmodel/service/game/GameModelMapper.java
@@ -10,40 +10,40 @@ import java.util.UUID;
 
 @Component
 public class GameModelMapper {
-    public AdminGameDto toAdminGameDto(GameModel game) {
-        return new AdminGameDto(
-                game.getGameId(),
-                game.getName(),
-                game.getDescription(),
-                game.getPictureUrl(),
-                game.getGameUrl(),
-                game.getCreatorName()
-               // game.getReviewState()
-               // game.getRules().stream()
-               //         .map(AdminGameDto.RuleDto::new)
-               //         .toList(),
-               // game.getAchievementEmbeddables().stream()
-               //         .map(a -> new AdminGameDto.GameAchievementDto(a.getCode(), a.getDescription()))
-               //         .toList(),
-               // game.isPlayableWithAI()
-        );
-    }
+//    public AdminGameDto toAdminGameDto(GameModel game) {
+//        return new AdminGameDto(
+//                game.getGameId(),
+//                game.getName(),
+//                game.getDescription(),
+//                game.getPictureUrl(),
+//                game.getGameUrl(),
+//                game.getCreatorName()
+//               // game.getReviewState()
+//               // game.getRules().stream()
+//               //         .map(AdminGameDto.RuleDto::new)
+//               //         .toList(),
+//               // game.getAchievementEmbeddables().stream()
+//               //         .map(a -> new AdminGameDto.GameAchievementDto(a.getCode(), a.getDescription()))
+//               //         .toList(),
+//               // game.isPlayableWithAI()
+//        );
+//    }
 
-    public PlayerGameDto toPlayerGamesDto(GameModel game, UUID favouriteGameId) {
-        return new PlayerGameDto(
-                game.getGameId(),
-                game.getName(),
-                game.getDescription(),
-                game.getPictureUrl(),
-                game.getGameUrl(),
-                game.getCreatorName()
-               // game.getRules().stream()
-               //         .map(PlayerGameDto.RuleDto::new)
-               //         .toList(),
-               // game.getGameId().equals(favouriteGameId),
-               // game.isPlayableWithAI()
-        );
-    }
+//    public PlayerGameDto toPlayerGamesDto(GameModel game, UUID favouriteGameId) {
+//        return new PlayerGameDto(
+//                game.getGameId(),
+//                game.getName(),
+//                game.getDescription(),
+//                game.getPictureUrl(),
+//                game.getGameUrl(),
+//                game.getCreatorName()
+//               // game.getRules().stream()
+//               //         .map(PlayerGameDto.RuleDto::new)
+//               //         .toList(),
+//               // game.getGameId().equals(favouriteGameId),
+//               // game.isPlayableWithAI()
+//        );
+//    }
 
     public PublicGameDto toPublicGameDto(GameModel game) {
         return new PublicGameDto(
