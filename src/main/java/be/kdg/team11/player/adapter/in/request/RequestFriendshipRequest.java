50d2d1d366262b4908cbde9e0a459be87d112a6c
Radu-Matei Enciu <radu-matei.enciu@student.kdg.be>
2025-12-24 00:00:18 +0100

    fixed request friendship

--- src/main/java/be/kdg/team11/player/adapter/in/request/RequestFriendshipRequest.java
+++ src/main/java/be/kdg/team11/player/adapter/in/request/RequestFriendshipRequest.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record RequestFriendshipRequest(
+        @NotNull(message = "Recipient username cannot be null")
         @NotEmpty(message = "Recipient username cannot be empty")
         String recipientUsername
 ) {}
