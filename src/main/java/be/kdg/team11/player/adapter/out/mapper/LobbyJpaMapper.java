982d6decc935880766a4a652a522198f177370dd
Radu-Matei Enciu <radu-matei.enciu@student.kdg.be>
2025-12-24 17:29:52 +0100

    Fixed lobby referencing full jpa entity

--- src/main/java/be/kdg/team11/player/adapter/out/mapper/LobbyJpaMapper.java
+++ src/main/java/be/kdg/team11/player/adapter/out/mapper/LobbyJpaMapper.java
@@ -12,20 +12,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class LobbyJpaMapper {
-    //TODO Make sure it is ok to inject this mapper into here
-    /// as long as the mappers are avoid circular dependencies (both of them calling each other) it is not violating any principles
-    /// its used often with mapstruct mappers
-    private final GameReferenceJpaMapper gameReferenceJpaMapper;
-
-    public LobbyJpaMapper(GameReferenceJpaMapper gameReferenceJpaMapper) {
-        this.gameReferenceJpaMapper = gameReferenceJpaMapper;
-    }
 
     public LobbyJpaEntity toJpaEntity(Lobby lobby) {
         LobbyJpaEntity entity = new LobbyJpaEntity();
 
         entity.setLobbyId(lobby.getLobbyId().lobbyId());
-        entity.setGameReference(gameReferenceJpaMapper.toJpaEntity(lobby.getGameReference()));
+        entity.setGameReference(lobby.getGameReference().gameId());
 
         SlotJpaEmbeddable slotJpaEmbeddable1 = new SlotJpaEmbeddable();
         SlotJpaEmbeddable slotJpaEmbeddable2 = new SlotJpaEmbeddable();
@@ -46,8 +38,8 @@ public class LobbyJpaMapper {
     }
 
     public Lobby toDomain(LobbyJpaEntity entity) {
-        LobbyId lobbyId = new LobbyId(entity.getLobbyId());
-        GameReference gameReference = gameReferenceJpaMapper.toDomain(entity.getGameReference());
+        LobbyId lobbyId = LobbyId.of(entity.getLobbyId());
+        GameReference gameReference = GameReference.of(entity.getGameReference());
 
         SlotJpaEmbeddable slotJpaEmbeddable1 = entity.getSlot1();
         SlotJpaEmbeddable slotJpaEmbeddable2 = entity.getSlot2();
